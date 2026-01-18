c5947a9f91a65ca08ea5b0c45de29f2c5f456095
Mohammad Alrashed <139542347+DatariusAI@users.noreply.github.com>
2026-01-18 18:43:06 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-/document.addEventListener("DOMContentLoaded", function () {
-  const links = document.querySelectorAll("nav a");
-  links.forEach(link => {
-    if (link.href === window.location.href) {
-      link.style.backgroundColor = "#D6DDE3";
-    }
-  });
-});
